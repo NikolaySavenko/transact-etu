602c6be1a27dc306d42ba3aef0a1ffe744d7c962
Савенко Николай Сергеевич <Nikolay.Savenko@geropharm.com>
2024-02-06 01:53:24 +0300

    first commit

---
+++ Bank/Program.cs
@@ -0,0 +1,98 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BankContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var api = app.MapGroup("/api");
+api.MapPost("/createDatabase", async (BankContext context) =>
+{
+    await context.Database.EnsureCreatedAsync();
+    return Results.Ok();
+});
+
+api.MapPost("/deleteDatabase", async (BankContext context) =>
+{
+    await context.Database.EnsureDeletedAsync();
+    return Results.Ok();
+});
+
+api.MapPost("/createAccount", async (CreateAccountCommand command, BankContext context) =>
+{
+    await context.Accounts.AddAsync(new Account { Id = command.Id, Balance = command.Balance });
+    await context.SaveChangesAsync();
+    return Results.Ok();
+});
+
+api.MapGet("/{id:guid}/balance", async (Guid id, BankContext context) =>
+{
+    var account = await context.Accounts.FindAsync(id);
+    return Results.Ok(account.Balance);
+});
+
+api.MapPost("/deposit", async (DepositCommand command, BankContext context) =>
+{
+    var account = await context.Accounts.Where(a => a.Id == command.Id).FirstOrDefaultAsync();
+    account.Balance += command.Amount;
+    await context.SaveChangesAsync();
+    return Results.Ok();
+});
+
+// transfer
+api.MapPost("/transfer", async (TransferCommand command, BankContext context) =>
+{
+    var account = await context.Accounts.FindAsync(command.From);
+    app.Logger.LogInformation($"From Account {account.Id} has balance {account.Balance}");
+    account.Balance -= command.Amount;
+    await context.SaveChangesAsync();
+
+    var toAccount = await context.Accounts.FindAsync(command.To);
+    app.Logger.LogInformation($"To Account {toAccount.Id} has balance {toAccount.Balance}");
+    toAccount.Balance += command.Amount;
+    await context.SaveChangesAsync();
+    return Results.Ok();
+});
+
+
+app.Run();
+
+record CreateAccountCommand(Guid Id, int Balance = 0);
+record DepositCommand(Guid Id, int Amount);
+record TransferCommand(Guid From, Guid To, int Amount);
+
+public class Account {
+    public Guid Id { get; set; }
+    public int Balance { get; set; }
+}
+
+public class BankContext : DbContext
+{
+    public BankContext(DbContextOptions<BankContext> options) : base(options) { }
+    public DbSet<Account> Accounts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.EnableSensitiveDataLogging();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>().ToTable("accounts", t => t.HasCheckConstraint("positive_balance", "balance >= 0"));
+        modelBuilder.Entity<Account>().Property(a => a.Balance).HasDefaultValue(0).HasColumnName("balance");
+        modelBuilder.Entity<Account>().Property(a => a.Id).HasColumnName("id");
+        modelBuilder.Entity<Account>().HasKey(a => a.Id);
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ TransactionGenerator/Program.cs
@@ -0,0 +1,81 @@
+﻿// Generate many csv files with transactions by this rules:
+// 1. Every file contains equal number of transactions
+// 2. Transaction is line in format "from,to,amount"
+// 3. Transaction 'from' is guid address of sender
+// 4. Transaction 'to' is guid address of receiver
+// 5. Transaction 'amount' is amount of money to be received
+// 6. User can be sender or receiver of transaction
+// 7. User balance is calculated by sequentially applying previous transactions
+// 8. User can be sender only if his balance is greater than zero
+// 9. Transaction amount can be less than user actual balance
+// 10. Users count and transactions count are provided from command line
+// 11. After all transactions are written to file program should generate a new file with users final balances into excpected.csv in format 'user,balance'
+
+using System.Diagnostics;
+
+var st = new Stopwatch();
+st.Start();
+
+// from command line
+var transactionCount = long.Parse(args[0]);
+var userCount = long.Parse(args[1]);
+
+// generate users
+var users = new Guid[userCount];
+for (int i = 0; i < users.Length; i++)
+{
+    users[i] = Guid.NewGuid();
+}
+
+// generate transactions
+var balances = new Dictionary<Guid, int>();
+
+// initialize balances
+foreach (var user in users)
+{
+    balances[user] = 1000;
+}
+
+// generate transactions
+var random = new Random();
+var filename = $"transactions.csv";
+using var transactionsWriter = new StreamWriter(filename);
+for (var i = 0; i < transactionCount; i++) {
+	// check if sender can send money
+	// user can be sender only in he have balance > 0
+	var sender = users[random.Next(0, users.Length)];
+	while (!balances.ContainsKey(sender) || balances[sender] <= 0)
+	{
+		sender = users[random.Next(0, users.Length)];
+	}
+
+	// generate receiver
+	var receiver = users[random.Next(0, users.Length)];
+	while (receiver == sender)
+	{
+		receiver = users[random.Next(0, users.Length)];
+	}
+
+	// generate amount
+	var amount = random.Next(1, balances[sender] + 1);
+
+	// update balances
+	balances[sender] -= amount;
+	balances[receiver] = balances.ContainsKey(receiver) ? balances[receiver] + amount : amount;
+
+	// add transaction
+	transactionsWriter.WriteLine($"{sender},{receiver},{amount}");
+	Console.WriteLine($"Transaction #{i} added {sender} to {receiver} amount {amount}");
+}
+
+// write final balances to file
+
+using (var writer = new StreamWriter("expected.csv"))
+{
+    foreach (var balance in balances)
+    {
+        writer.WriteLine($"{balance.Key},{balance.Value}");
+    }
+}
+
+Console.WriteLine($"Done in {st.ElapsedMilliseconds} ms");
